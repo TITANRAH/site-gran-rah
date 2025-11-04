fb638ff22fa523322b0ab5638eabddb800a27e03
TITANRAH <granrah1@gmail.com>
2025-11-03 21:34:23 -0300

    const number whatsapp and slide cd's

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const WHATSAPP_NUMBER = "56949260725";
+
+export const CONTACT_INFO = {
+  email: "contacto@granrah.cl",
+  location: "Santiago, Chile",
+  whatsapp: WHATSAPP_NUMBER,
+} as const;
+
+export const SOCIAL_LINKS = {
+  spotify: "https://open.spotify.com/artist/6JjrF0EnCW3Ylj9gj3FXWZ",
+  instagram: "https://www.instagram.com/granrah/",
+  youtube: "https://www.youtube.com/@granrah",
+  facebook: "https://www.facebook.com/granrah",
+} as const;
